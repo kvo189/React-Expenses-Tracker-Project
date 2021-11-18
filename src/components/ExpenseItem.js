726e07d55b7ea33519d28daf480788aa5562236b
Khang Q Vo <kvo189@uab.edu>
2021-11-18 14:14:20 -0600

    add components from an array of expenses

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 253.32;
+function ExpenseItem(props) {
+  const expenseDate = props.date.toDateString();
+  const expenseTitle = props.title;
+  const expenseAmount = props.amount;
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toDateString()}</div>
+      <div>{expenseDate}</div>
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
