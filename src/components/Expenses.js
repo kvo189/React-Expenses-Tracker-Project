db492cc04e297718ccb29754f45c5975fb83a7c2
Khang Q Vo <kvo189@uab.edu>
2021-11-20 11:26:34 -0600

    Add Card as a wrapper component for composition

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,13 +1,14 @@
 import "./Expenses.css";
+import Card from './Card.js';
 import ExpenseItem from "./ExpenseItem";
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {props.expenses.map((expense) => (
         <ExpenseItem key={expense.id} amount={expense.amount} date={expense.date} title={expense.title}></ExpenseItem>
       ))}
-    </div>
+    </Card>
   );
 }
 
